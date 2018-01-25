b9d11e96d565cbe963436d1e84795bf4b4f13bb8
Alejandro Cremades <alecrem@users.noreply.github.com>
2018-01-25 14:24:40 +0900

    The trailing slash was making a new directory on the host

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-jekyll b && rsync -avr -e 'ssh -p 2222' _site/ punyu.jp-studiokura@ssh494.lolipop.jp:~/web/artsitoya.com/
+jekyll b && rsync -av -e 'ssh -p 2222' _site/ punyu.jp-studiokura@ssh494.lolipop.jp:~/web/artsitoya.com
