4249f0dccd65e2397c56f68ff4a37960d585ce9a
Alejandro Cremades <alecrem@users.noreply.github.com>
2018-01-25 11:49:58 +0900

    Switching to rsync for deploy. Faster, non redundant sync, and syncs dotfiles by default

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-jekyll b && scp -r -P2222 _site/* punyu.jp-studiokura@ssh494.lolipop.jp:~/web/artsitoya.com/
+jekyll b && rsync -avr -e 'ssh -p 2222' _site/ punyu.jp-studiokura@ssh494.lolipop.jp:~/web/artsitoya.com/
